55b2a560d03d75da5928f6635c32b2ce879ed333
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-05-03 23:34:59 +0530

    Daily Temperatures- Leetcode

--- ADS-2/DailyTemperatures.java
+++ ADS-2/DailyTemperatures.java
@@ -36,3 +36,26 @@ class DailyTemperatures {
         System.out.println("]");
     }
 }
+
+
+/*
+ * Solution using ArrayDeque
+ * class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
+
+ */
