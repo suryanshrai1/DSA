50f9bde4bc4c098b7406d48ed8e6f0ba9b01ef6c
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2025-10-06 23:13:01 +0530

    SudokuValidator- leetcode

---
+++ ADS-2/SudokuValidator.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+
+public class SudokuValidator {
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int boxIdx = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].contains(c) || cols[j].contains(c) || boxes[boxIdx].contains(c)) {
+                    return false;
+                }
+
+                rows[i].add(c);
+                cols[j].add(c);
+                boxes[boxIdx].add(c);
+            }
+        }
+
+        return true;
+    }
+
+    // Sample main method
+    public static void main(String[] args) {
+        SudokuValidator validator = new SudokuValidator();
+
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        boolean result = validator.isValidSudoku(board);
+        System.out.println("Is the Sudoku board valid? " + result);
+    }
+}
