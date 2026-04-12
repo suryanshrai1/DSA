9c0863f5a264beb54eea238f046085781873b144
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2026-04-12 23:26:10 +0530

    toplitzMatrix-gfg NPCI

---
+++ NPCI GFG/toplitzMatrix.java
@@ -0,0 +1,12 @@
+public class toplitzMatrix {
+    public boolean isToeplitz(int[][] mat) {
+        for (int i = 1; i < mat.length; i++) {
+            for (int j = 1; j < mat[0].length; j++) {
+                if (mat[i][j] != mat[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
