4d5a35e423b2f8e32d4064a8b51d4503fc1fc2f8
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2026-04-17 23:20:36 +0530

    anagramPalindrome-gfg NPCI

---
+++ NPCI GFG/anagramPalindrome.java
@@ -0,0 +1,15 @@
+public class anagramPalindrome {
+    boolean canFormPalindrome(String s) {
+        // code here
+        Set<Character> st = new HashSet<>();
+        for(char ch : s.toCharArray()){
+            if(st.contains(ch)){
+                st.remove(ch);
+            }
+            else{
+                st.add(ch);
+            }
+        }
+        return st.size() <= 1 ? true : false;
+    }
+}
