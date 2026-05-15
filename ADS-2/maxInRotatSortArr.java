a0aa8c26996c7332ca4cfd6a2dbb73f0f493a972
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2026-05-15 23:33:28 +0530

    maxInRotatSortArr -leetcode potd

---
+++ ADS-2/maxInRotatSortArr.java
@@ -0,0 +1,18 @@
+public class maxInRotatSortArr {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int lo=0;
+        int hi=n-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid] > nums[hi]){
+                //pivot -> in right half
+                lo=mid+1;
+            }
+            else{
+                hi=mid;
+            }
+        }
+        return nums[lo];
+    }
+}
