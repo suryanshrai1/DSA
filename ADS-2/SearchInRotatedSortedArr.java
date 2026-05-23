0a062286bd67ee5ed319c54a9c44939e232b9a9c
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2026-05-23 22:23:12 +0530

    SearchInRotatedSortedArr- leetcode potd

--- ADS-2/SearchInRotatedSortedArr.java
+++ ADS-2/SearchInRotatedSortedArr.java
@@ -1,3 +1,14 @@
 public class SearchInRotatedSortedArr {
-    
+    public boolean check(int [] nums){
+        int n=nums.length;
+        if(n==0) return false;
+        if(n==1) return true;
+        int breakPoint =0;
+        for(int i=0;i<n;i++){
+            if(nums[i] > nums[(i+1)%n]){
+                breakPoint++;
+            }
+        }
+        return breakPoint <=1;
+    }
 }
