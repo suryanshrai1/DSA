bfb7c8b3d3ede0f80829fbf29239734651011ed1
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2026-06-01 00:00:47 +0530

    equalRowColPairs -leetcode potd

---
+++ ADS-2/eqRowCol.java
@@ -0,0 +1,13 @@
+class equalRowColPairs {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        int ans = 0;
+        Map<List<Integer>, Integer> map = new HashMap<>();
+
+        for(int r=0;r<n;r++){
+            List<Integer> row= new ArrayList<>();
+            
+        }
+        return ans;
+    }
+}
